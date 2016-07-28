1bda7f0a015dd9606454233d8cabb9272338799d
ali94s <ali_netmail@163.com>
2016-07-28 14:42:16 +0800

    tcp服务器 熟悉创建tcp服务器的步骤

--- tcp/client.c
+++ tcp/client.c
@@ -1,8 +1,66 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <sys/types.h>      
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
 
+#define _SIZE_  1024
+#define _PORT_ 8080
+#define _IP_ "127.0.0.1"
 
 int main()
 {
-	printf("client...\n");
+	int  server_sock = socket(AF_INET,SOCK_STREAM,0); // IPV4 , 流式套接字 
+	if(server_sock < 0)
+	{
+		perror("socket");
+	}
+
+	struct sockaddr_in local;
+	local.sin_family = AF_INET;       
+	local.sin_port = htons(_PORT_);   //主机字节序列向网络字节序列转换
+	local.sin_addr.s_addr =inet_addr(_IP_);
+
+	socklen_t addrlen = sizeof(local);
+
+	if(connect(server_sock, (struct sockaddr *)&local, addrlen) < 0)
+	{
+		perror("connect");
+		close(server_sock);
+		return 1;
+	}
+
+	char buf[_SIZE_];
+	int done = 0;
+
+	while(!done)
+	{
+		memset(buf,'\0',sizeof(buf));
+		printf("Please Enter :");
+		fflush(stdout);
+		int size = read(0,buf,sizeof(buf)-1);
+		if(size < 0)
+		{
+			perror("read");
+			return 2;
+		}
+		buf[size-1] = '\0';
+		write(server_sock,buf,size);
+		if(strcmp(buf,"quit") == 0)
+		{
+			break;
+		}
+		
+		memset(buf,'\0',sizeof(buf));
+		read(server_sock,buf,sizeof(buf));
+		printf("server : %s\n",buf);
+		fflush(stdout);
+	}
+
+	close(server_sock);
+
 	return 0;
 }
+
+
--- tcp/server.c
+++ tcp/server.c
@@ -7,15 +7,35 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<stdlib.h>
-//#include<sys/>
+#include<pthread.h>
+#include<sys/wait.h>
 
 const char *ip="127.0.0.1";
 const short port=8080;
 #define _SIZE_ 1024
 
-void *running(void *arg)
+void *running(void* arg)
 {
-	
+	int fd=(int)arg;
+	char buf[_SIZE_];
+	while(1)
+	{
+		memset(buf,'\0',sizeof(buf));
+		int s=read(fd,buf,sizeof(buf)-1);
+		if(s>0)
+		{
+			buf[s-1]='\0';
+			printf("client#%s\n",buf);
+			fflush(stdout);
+			write(fd,buf,s);
+		}
+		else
+		{
+			perror("read");
+			return NULL;
+		}
+	}
+	return (void *)0;
 }
 
 
@@ -29,6 +49,11 @@ int main()
 		perror("socket");
 		return 1;
 	}
+	
+//	int opt=1;
+//	socklen_t optlen=sizeof(opt);
+//	//不经历TIME_WAIT时间
+//	setsockopt(socket,SOL_SOCKET,SO_REUSEADDR,&opt,optlen);
 
 	//初始化本地套接字  并绑定ip和端口
 	struct sockaddr_in server_socket;
@@ -40,6 +65,7 @@ int main()
 	if(bind(sock,(struct sockaddr*)&server_socket,sizeof(struct sockaddr_in))<0)
 	{
 		perror("bind");
+		close(socket);
 		return 2;
 	}
 	
@@ -47,6 +73,7 @@ int main()
 	if(listen(sock,10)<0)
 	{
 		perror("listen");
+		close(socket);
 		return 3;
 	}
 
@@ -54,54 +81,19 @@ int main()
 	{
 		struct sockaddr_in peer;
 		socklen_t len=sizeof(peer);
-		while(1)
+		int fd=accept(sock,(struct sockaddr*)&peer,&len);
+		if(fd<0)
 		{
-			int fd=accept(sock,(struct sockaddr*)&peer,&len);
-			if(fd<0)
-			{
-				perror("accept");
-				close(sock);
-				return 4;
-			}
-			printf("new user:%s,%d\n",inet_ntoa(peer.sin_addr),ntohs(peer.sin_port));
-
-			//创建线程来各司其职
-			pthread_t id;
-			pthread_create(&id,NULL,running,NULL);
-
-
-		//	pid_t id=fork();
-		//	if(id==0)
-		//	{
-		//		while(1)
-		//		{
-		//			char buf[_SIZE_];
-		//			memset(buf,'\0',sizeof(buf));
-		//			size_t s=read(fd,buf,sizeof(buf)-1);
-		//			if(s>0)
-		//			{
-		//				printf("client:#%s",buf);
-		//				fflush(stdout);
-		//				memset(buf,'\0',sizeof(buf));	
-		//				//buf[strlen(buf)-1]='\0';
-		//				//fgets(buf,sizeof(buf),stdin);
-		//				//printf("server:#%s",buf);
-		//				write(fd,buf,strlen(buf));
-		//			}
-		//			else
-		//			{
-		//				perror("read");
-		//				return 5;
-		//			}
-		//		}
-		//		exit(0);
-		//	}
-		//	else
-		//	{
-		//		pid_t ret=waitpid(-1,NULL,WNOHANG);
-		//			
-		//	}
+			perror("accept");
+			close(sock);
+			return 4;
 		}
+		printf("new user:%s,%d\n",inet_ntoa(peer.sin_addr),ntohs(peer.sin_port));
+
+		//创建线程来各司其职
+		pthread_t id;
+		pthread_create(&id,NULL,running,(void *)fd);
+		pthread_detach(id);
 	}
 
 	close(sock);
